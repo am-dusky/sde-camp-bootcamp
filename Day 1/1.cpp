d2506ae15c823b49ecba64ba703eecf061dbe47b
Megha Singh <81347179+am-dusky@users.noreply.github.com>
2022-02-02 21:42:19 +0530

    Update 1.cpp

--- Day 1/1.cpp
+++ Day 1/1.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+	int n;
+	double d;
+	string s;
+	
+	cout<<"Enter an integer: ";
+	cin>>n;
+	
+	cout<<"Enter a double: ";
+	cin>>d;
+	
+	cout<<"Enter a string: ";
+	cin>>s;
+	
+	cout<<"\nInteger: "<<n;
+	cout<<"\nDouble: "<<d;
+	cout<<"\nString: "<<s;
+	
+	return 0;
+}
