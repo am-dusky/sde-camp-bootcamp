5f83cf0482dc121d4b704378084325bcfe338345
Megha Singh <81347179+am-dusky@users.noreply.github.com>
2022-02-02 21:50:39 +0530

    Create 3.cpp

---
+++ Day 1/3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Using for loop: \n";
+	for(int i=1;i<=100;i++){
+		cout<<i<<" ";
+	}
+	cout<<"\nusing while loop: \n";
+	int i=1;
+	while(i<=100){
+		cout<<i<<" ";
+		i++;
+	}
+	return 0;
+}
