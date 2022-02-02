506292422cc4c27baf54e63fb36c06fed41363a5
Megha Singh <81347179+am-dusky@users.noreply.github.com>
2022-02-02 22:08:35 +0530

    Create 7.cpp

---
+++ Day 1/7.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int sum=0;
+	int n;
+	cout<<"Enter the value of n: ";
+	cin>>n;
+	
+	for(int i=1;i<=n;i++){
+		sum+=i;
+	}
+	
+	cout<<"Sum of "<<n<<" natural numbers is: "<<sum;
+	return 0;
+}
