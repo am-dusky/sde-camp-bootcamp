6668cb03e4a48e7a472af65a52f2fbf07cdfc27f
Megha Singh <81347179+am-dusky@users.noreply.github.com>
2022-02-02 21:47:36 +0530

    Create 2.cpp

---
+++ Day 1/2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int age;
+	cout<<"Enter your age: ";
+	cin>>age;
+	if(age>18){
+		cout<<"Age is greater than 18 ";
+	}
+	else{
+		cout<<"Age is less than 18 ";
+	}
+	return 0;
+}
