6bdc987da8636dcecfcef2938af8df35daf59580
Megha Singh <81347179+am-dusky@users.noreply.github.com>
2022-02-03 20:46:44 +0530

    Create 6.cpp

---
+++ Day2/6.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Enter the number of rows: ";
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		for(int j=n;j>=1;j--){
+			if(i>=j){
+				cout<<"* ";
+			}
+			else{
+				cout<<" ";
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+/*
+Enter the number of rows: 5
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+*/
