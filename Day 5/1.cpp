86bf7d1a3567486383291cd31bef5410f62bdc89
Megha Singh <81347179+am-dusky@users.noreply.github.com>
2022-02-10 11:54:39 +0530

    Create 1.cpp

---
+++ Day 5/1.cpp
@@ -0,0 +1,16 @@
+ void p(int index,vector<int>& nums,vector<vector<int>>&ans){
+        if(index==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            p(index+1,nums,ans);
+            swap(nums[index],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>arr;
+        p(0,nums,arr);
+        return arr;
+    }
